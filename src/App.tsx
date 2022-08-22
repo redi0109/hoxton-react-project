10ee3788c6f85f188bda87063e46c286534942f3
Demir Boraj <demirboraj97@gmail.com>
2022-08-22 16:29:40 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,37 @@
-import './App.css';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import "./App.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 function App() {
-
-
   return (
     <div className="App">
-    <header className='header'>
-      <div className='logo'>
-       <img className='logo-img'  src='../public/images/logo.png' >
-        </img>
-      </div>
-      <div className='menu'>
-        <ul>
-          <li>Pizza</li>
-          <li>Samdwiches</li>
-          <li>Salads</li>
-          <li>Lunch</li>
-          <li>Drinks</li>
-          <li>Beers</li>
+      <header className="header">
+        <div className="logo">
+          <img className="logo-img" src="../public/images/logo.png"></img>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>Pizza</li>
+            <li>Sandwiches</li>
+            <li>Salads</li>
+            <li>Lunch</li>
+            <li>Drinks</li>
+            <li>Beers</li>
           </ul>
-         
-      </div>
-      <div className='search-bar'>
-        <input type='text' placeholder='Search'></input>
-      </div>
-      <div className='cart'>
-        <ShoppingCartIcon/>
-      </div>
-    </header>
-    <main className='main'>
+        </div>
 
-    </main>
-    <footer className='footer'>
-      </footer>
+        <div className="search">
+          <input className="search-bar" type="text" placeholder="your favorite pizza..."></input>
+        </div>
+
+        <div className="cart">
+          <ShoppingCartIcon color="error" fontSize="large" />
+        </div>
+      </header>
+      <main className="main">
+        <h1>Alo 127, një picë se mbarova</h1>
+      </main>
+      <footer className="footer"></footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
