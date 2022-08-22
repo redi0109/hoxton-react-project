8da573cfba02e321e61dea25bddeb0a1498e1320
Demir Boraj <demirboraj97@gmail.com>
2022-08-22 19:45:38 +0200

    creating db

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,47 +1,47 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { NavLink, Link, Router } from "react-router-dom";
 import "../App.css";
+import { Pizza } from "../pages/Pizza";
 
-export function Header (){
-    return(
-        <header className="header">
-        <div className="logo">
-          <img className="logo-img" src="../public/images/logo.png"></img>
-        </div>
-        <div className="menu">
-          <ul>
-            <li className="menu-item">
-              <Link to='/pizza'>Pizza</Link>
-              </li>
-              <li className="menu-item">
-              <Link to='/sandwiches'>Sandwiches</Link>
-              </li>
-              <li className="menu-item">
-              <Link to='/salads'>Salads</Link>
-              </li>
-              <li className="menu-item">
-              <Link to='/lunch'>Lunch</Link>
-              </li>
-              <li className="menu-item">
-              <Link to='/drinks'>Drinks</Link>
-              </li>
-              <li className="menu-item">
-              <Link to='/beers'>Beers</Link>
-              </li>
-          </ul>
-        </div>
+export function Header() {
+  const [categories, setCategories] = useState([]);
 
-        <div className="search">
-          <input
-            className="search-bar"
-            type="text"
-            placeholder="your favorite pizza..."
-          ></input>
-        </div>
+  useEffect(() => {
+    fetch("http://localhost:3001/categories")
+      .then((resp) => resp.json())
+      .then((categoriesFromServer) => setCategories(categoriesFromServer));
+  }, []);
 
-        <div className="cart">
-          <ShoppingCartIcon color="error" fontSize="large" cursor='pointer'/>
-        </div>
-      </header>
-    )
+  return (
+    <header className="header">
+      <div className="logo">
+        <img className="logo-img" src="../public/images/logo.png"></img>
+      </div>
+      <div className="menu">
+        <ul>
+          {categories.map((category) => (
+          <li className="menu-item"> <a href="#">{category.title}</a></li>
+          ))}
+         
+        </ul>
+      </div>
+
+      <div className="search">
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="your favorite pizza..."
+        ></input>
+      </div>
+
+      <div className="cart">
+        <ShoppingCartIcon color="error" fontSize="large" cursor="pointer" />
+      </div>
+    </header>
+  );
+}
+
+function category(category: any): void | PromiseLike<void> {
+  throw new Error("Function not implemented.");
 }
---
+++ src/pages/Pizza.tsx
@@ -0,0 +1,9 @@
+export function Pizza (){
+    return(
+        <div>
+            <div className="category-name"> 
+            <h1>Pizza</h1>
+            </div>
+        </div>
+    )
+}
