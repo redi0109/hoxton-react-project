0c14ac3ee5a4112b8496a974f1f6ed74dab5c383
Demir Boraj <demirboraj97@gmail.com>
2022-08-23 15:54:24 +0200

    1

--- src/pages/Pizza.tsx
+++ src/pages/Pizza.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import "../App.css";
 
+
 export function Pizza (){
     const [pizzas, setPizzas] = useState([]);
     
-    useEffect(() => {
+   
     function getDatafromServer(){
         fetch("http://localhost:3001/pizzas")
         .then ((resp) => resp.json() )
         .then ((data) => setPizzas(data));
     }
+   
+    useEffect(() => {
+        getDatafromServer();
     }, []);
 
 
@@ -19,13 +23,14 @@ export function Pizza (){
             <div className="category-name"> 
             <h1>Pizza</h1>
             {pizzas.map((pizza) => 
-            <>
+            <div className="products-section">
               <div className="product">
                 <img className="product-img" src={pizza.image}/>
-                <h2 className="product-name">{pizza.name}</h2>
-            </div>
+                <h2 className="product-name">{pizza.name} </h2>
+                </div>
+           
 
-            </>
+            </div>
             )}
           
 
